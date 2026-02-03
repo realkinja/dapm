aa88958259e39ed2bfb34d195dbaa361b9e222af
realkinja <78376161+realkinja@users.noreply.github.com>
2026-02-03 12:42:19 +0100

    ✨ CLI Arguments

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,33 @@
 mod app;
+mod cli;
 mod dialog;
 mod ollama;
 mod ui;
-use crate::app::App;
+use clap::Parser;
+use reqwest::dns::Name;
+
+use crate::{app::App, cli::Cli, ollama::Ollama};
 use std::io;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let mut app = App {
-        master_prompt: include_str!("../master-prompt.md").to_string(),
-        ..Default::default()
+    let cli = Cli::parse();
+    let mut app = {
+        if let Some(custom_model) = cli.model {
+            App {
+                master_prompt: include_str!("../master-prompt.md").to_string(),
+                ollama: Ollama {
+                    model: custom_model,
+                    ..Default::default()
+                },
+                ..Default::default()
+            }
+        } else {
+            App {
+                master_prompt: include_str!("../master-prompt.md").to_string(),
+                ..Default::default()
+            }
+        }
     };
 
     let mut terminal = ratatui::init();
